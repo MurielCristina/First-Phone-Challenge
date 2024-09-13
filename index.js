b3b70fa89256839f582edec648836246ca836c3d
Muri <mcsanchezlaguna@gmail.com>
2024-09-13 16:23:22 +0200

    change screen function

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ dateElement.innerHTML = moment().format("d/MM/YY");
 
 let timeElement = document.querySelector("#add-time");
 timeElement.innerHTML = moment().format("hh:mm a");
+
+function moreButton() {
+  document.querySelector("#screen-one").style.display = "none";
+  document.querySelector("#screen-two").style.display = "block";
+}
+
+function homeButton() {
+  document.querySelector("#screen-two").style.display = "none";
+  document.querySelector("#screen-one").style.display = "block";
+}
+
+document.querySelector("#more-button").addEventListener("click", moreButton);
+document.querySelector("#home-button").addEventListener("click", homeButton);
